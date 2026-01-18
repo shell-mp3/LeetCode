bf4d5ee8cbe9a652fcd50abc9499f64adbf94e8b
Shelly <shelly.normatov@gmail.com>
2026-01-18 15:38:25 -0500

    Time: 238 ms (52.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,4 @@
+SELECT machine_id,
+ROUND(SUM(CASE WHEN activity_type = 'start' THEN -timestamp ELSE timestamp END) / COUNT(DISTINCT process_id), 3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
